079a3a9207ce245ce1089a53796d9255632ab874
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-07-29 21:15:18 -0600

    add basic express server

---
+++ backend-node/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('BudgeBuddyapp Node API is running');
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
