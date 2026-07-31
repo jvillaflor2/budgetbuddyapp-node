39cb492b5a056f0e88b3e07e7c510102908f5355
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-07-30 19:51:40 -0600

    add GET /categories route

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-
+const categoriesRouter = require('./routes/categories');
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/categories', categoriesRouter);app.use('/categories', categoriesRouter);
 
 app.get('/', (req, res) => {
     res.send('BudgeBuddyapp Node API is running');
