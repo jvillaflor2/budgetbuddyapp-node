08be5d588ceb93c1ce65ecdc6beaa15c95923716
jvillaflor2 <157553901+jvillaflor2@users.noreply.github.com>
2026-07-31 13:29:54 -0600

    set up transaction server and added get all transactions

---
+++ backend-node/routes/transactions.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db');
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM transactions ORDER by date DESC');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error:'Failed to fetch transactions'});
+    }
+});
+
+module.exports = router;
--- backend-node/server.js
+++ backend-node/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const categoriesRouter = require('./routes/categories');
+const transactionsRouter = require('./routes/transactions');
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/categories', categoriesRouter);app.use('/categories', categoriesRouter);
+app.use('/transactions', transactionsRouter);
 
 app.get('/', (req, res) => {
     res.send('BudgeBuddyapp Node API is running');
